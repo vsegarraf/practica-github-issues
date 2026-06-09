5064a05c66bf6977d6dbad4498047b37926b37d3
vsegarraf <50122414+vsegarraf@users.noreply.github.com>
2026-06-09 16:27:46 -0400

    Update test_calculadora.py

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,11 +1,11 @@
 # tests/test_calculadora.py
-import scr.calculadora
+import src.calculadora
 
 def test_sumar_dos_numeros():
-    assert scr.calculadora.sumar(2, 3) == 5
+    assert src.calculadora.sumar(2, 3) == 5
 
 def test_restar():
-    assert scr.calculadora.restar(5, 3) == 2
+    assert src.calculadora.restar(5, 3) == 2
     
 def test_multiplicar():
-    assert scr.calculadora.multiplicar(4, 2) == 8
+    assert src.calculadora.multiplicar(4, 2) == 8
