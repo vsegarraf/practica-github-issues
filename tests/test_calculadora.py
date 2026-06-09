c431225d01f6f69788044e624371e49f09dfd560
vsegarraf <50122414+vsegarraf@users.noreply.github.com>
2026-06-09 14:35:59 -0400

    Create test_calculadora.py

---
+++ tests/test_calculadora.py
@@ -0,0 +1,11 @@
+# tests/test_calculadora.py
+import src.calculadora
+
+def test_sumar_dos_numeros():
+    assert src.calculadora.sumar(2, 3) == 5
+
+def test_restar():
+    assert src.calculadora.restar(5, 3) == 2
+    
+def test_multiplicar():
+    assert src.calculadora.multiplicar(4, 2) == 8
